de1d20c62e9bd68e0ef7a34abc7694f83656c82c
handongqingxue <1417140290@qq.com>
2022-10-24 15:10:14 +0800

    完善

--- src/main/java/com/sdwhNrcc/util/ServerReceiver.java
+++ src/main/java/com/sdwhNrcc/util/ServerReceiver.java
@@ -3,18 +3,26 @@ package com.sdwhNrcc.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-//import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+
+import com.alibaba.fastjson.JSON;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 //https://blog.csdn.net/young_YangY/article/details/84446559/
+//https://www.codenong.com/cs106100031/
 
 @Component
 public class ServerReceiver {
 
 	private static final Logger log=LoggerFactory.getLogger(ServerReceiver.class);
 	
-	/*
-	@rabb
-	public void process() {
-		System.out.println("111111111111");
+	@RabbitHandler
+	@RabbitListener(queues="tenant_msg_F4A1D30F_sc22050664")
+	public void process(Message message) {
+		
+		String str = new String(message.getBody());
+		//JSON.parseObject();
+		System.out.println("str==="+str);
 	}
-	*/
 }
